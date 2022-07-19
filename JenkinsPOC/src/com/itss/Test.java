34ffec692094488337947449e2b18f7d0ed20871
Allam Castillo <97133236+Allam-C@users.noreply.github.com>
2022-07-19 09:12:20 -0600

    POCs Repo

---
+++ JenkinsPOC/src/com/itss/Test.java
@@ -0,0 +1,19 @@
+package com.itss;
+
+/**
+ * TODO: Document me!
+ *
+ * @author allam
+ *
+ */
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Testing 1, 2, 3 ... Testing");
+    }
+
+}
