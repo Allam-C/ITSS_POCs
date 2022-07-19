13794e9798c577c5d3f970ad912f23547a169d1f
Allam Castillo <97133236+Allam-C@users.noreply.github.com>
2022-07-19 10:23:41 -0600

    Java Test - text file write

--- JenkinsPOC/src/com/itss/Test.java
+++ JenkinsPOC/src/com/itss/Test.java
@@ -1,5 +1,11 @@
 package com.itss;
 
+import java.io.BufferedWriter;
+//import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * TODO: Document me!
  *
@@ -14,6 +20,17 @@ public class Test {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Testing 1, 2, 3 ... Testing");
+        String filename = "module.xml";
+        try {
+            FileWriter fw = new FileWriter(filename,true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            pw.println("<text>Writing...</text>");
+            pw.close();
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }        
     }
 
 }
