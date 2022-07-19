fc0f2e0dcf0a0b37f209e8e8890d44b58f88ca4e
Allam Castillo <97133236+Allam-C@users.noreply.github.com>
2022-07-19 14:07:48 -0600

    xml file write

--- JenkinsPOC/src/com/itss/Test.java
+++ JenkinsPOC/src/com/itss/Test.java
@@ -1,10 +1,17 @@
 package com.itss;
 
-import java.io.BufferedWriter;
-//import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * TODO: Document me!
@@ -17,20 +24,46 @@ public class Test {
     /**
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
         // TODO Auto-generated method stub
-        System.out.println("Testing 1, 2, 3 ... Testing");
-        String filename = "module.xml";
-        try {
-            FileWriter fw = new FileWriter(filename,true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-            pw.println("<text>Writing...</text>");
-            pw.close();
-          } catch (IOException e) {
-            System.out.println("An error occurred.");
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        
+        Document doc = docBuilder.newDocument();
+        Element moduleRoot = doc.createElement("module");
+        moduleRoot.setAttribute("xmlns", "urn:jboss:module:1.0");
+        moduleRoot.setAttribute("name", "com.temenos.t24");
+        doc.appendChild(moduleRoot);
+
+        
+        Element resources = doc.createElement("resources");
+        Element resource = doc.createElement("resource-root");
+        moduleRoot.appendChild(resources);
+        resources.appendChild(resource);
+        resource.setAttribute("path", "path/to/lib");
+        
+        moduleRoot.appendChild(doc.createElement("dependencies"));
+        
+        try (FileOutputStream output = new FileOutputStream("module.xml")) {
+            writeXml(doc,output);
+        }
+        catch (IOException e) {
             e.printStackTrace();
-          }        
+        }
+    }
+
+    /**
+     * @param doc
+     * @param output
+     */
+    private static void writeXml(Document doc, FileOutputStream output) throws TransformerException{
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(output);
+        
+        transformer.transform(source, result);
+        
     }
 
 }
